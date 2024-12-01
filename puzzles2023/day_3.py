aebd46175ef4bc2232891d0309447124b75f08fb
nihilok <mjfullstack@gmail.com>
2024-12-01 23:58:02 +0000

    updates to 2023 day 3

--- puzzles2023/day_3.py
+++ puzzles2023/day_3.py
@@ -38,78 +38,48 @@ symbols = [s for s in punctuation if s != "."]
 
 def is_part_number(data, line_index, number_start, number_end):
     current_line = data[line_index]
-    
-    if number_start > 0:
-        slice_start = number_start - 1
-    else:
-        slice_start = 0
 
-    if number_end < len(current_line) - 2:
-        slice_end = number_end + 1
-    else:
-        slice_end = len(current_line) - 1
-    
-    if current_line[slice_start] in symbols:
+    # Check symbols in current line
+    if number_start > 0 and current_line[number_start - 1] in symbols:
         return True
-    if current_line[slice_end] in symbols:
+    if number_end < len(current_line) - 1 and current_line[number_end + 1] in symbols:
         return True
-    
-    previous_line = "NO PREVIOUS LINE"
-    next_line = "NO NEXT LINE"
-    section_above = ""
-    section_below = ""
 
+    # Check symbols in the line above
     if line_index > 0:
         previous_line = data[line_index - 1]
-    
-        section_above = previous_line[slice_start:slice_end + 1]
+        if any(c in symbols for c in previous_line[number_start:number_end + 1]):
+            return True
 
-        for c in section_above:
-            if c in symbols:
-                return True
-    
-    if line_index < len(data) - 2:
+    # Check symbols in the line below
+    if line_index < len(data) - 1:
         next_line = data[line_index + 1]
+        if any(c in symbols for c in next_line[number_start:number_end + 1]):
+            return True
 
-        section_below = next_line[slice_start:slice_end + 1]
-
-        for c in section_below:
-            if c in symbols:
-                return True
-
-    print("======NOT A PART NUMBER======")
-    print(slice_start, slice_end)
-    print(previous_line)
-    print(current_line)
-    print(next_line)
-    print(section_above)
-    print(current_line[number_start:number_end + 1])
-    print(section_below)
     return False
-
 
 def part_1(data):
     data = data.split("\n")
     part_numbers = []
     for line_index, line in enumerate(data):
         num_start = None
-        num_end = None
-        in_number = False
         for i, c in enumerate(line):
-            if in_number and c.isnumeric():
-                continue
-
-            if in_number and not c.isnumeric():
-                in_number = False
-                num_end = i - 1
-                if is_part_number(data, line_index, num_start, num_end):
-                    part_numbers.append(int(line[num_start:num_end + 1]))
-                
-            if not in_number and c.isnumeric():
-                num_start = i
-                in_number = True
+            if c.isnumeric():
+                if num_start is None:
+                    num_start = i
+            else:
+                if num_start is not None:
+                    num_end = i - 1
+                    if is_part_number(data, line_index, num_start, num_end):
+                        part_numbers.append(int(line[num_start:num_end + 1]))
+                    num_start = None
+        # Check if a number continues till the end of the line
+        if num_start is not None:
+            num_end = len(line) - 1
+            if is_part_number(data, line_index, num_start, num_end):
+                part_numbers.append(int(line[num_start:num_end + 1]))
     return sum(part_numbers)
-
 
 if __name__ == "__main__":
     puzzle_input = get_puzzle_input(day=3, year=2023)
