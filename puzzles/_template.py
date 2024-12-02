1c5c8733cd3c2b449df727fd8b9391f7fc22741c
nihilok <mjfullstack@gmail.com>
2024-12-02 00:25:24 +0000

    add starter template for all days

---
+++ puzzles/_template.py
@@ -0,0 +1,20 @@
+from aoc_utils import get_puzzle_input
+
+
+def parse_input(puzzle_input):
+    pass
+
+
+def part_1(puzzle_input):
+    pass
+
+
+def part_2(puzzle_input):
+    pass
+
+
+if __name__ == "__main__":
+    DAY = 0 # Change day to the current day
+    puzzle_input = get_puzzle_input(DAY).strip()
+    print(f"{part_1(puzzle_input)=}")
+    print(f"{part_2(puzzle_input)=}")
