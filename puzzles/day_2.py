6c19beb34060e14e74ca17652cd6bd23c9c7bbd1
nihilok <mjfullstack@gmail.com>
2024-12-02 00:21:21 +0000

    ready for day 2!

---
+++ puzzles/day_2.py
@@ -0,0 +1,19 @@
+from aoc_utils import get_puzzle_input
+
+
+def parse_input(puzzle_input):
+    pass
+
+
+def part_1(puzzle_input):
+    pass
+
+
+def part_2(puzzle_input):
+    pass
+
+
+if __name__ == "__main__":
+    puzzle_input = get_puzzle_input(2).strip()
+    print(f"{part_1(puzzle_input)=}")
+    print(f"{part_2(puzzle_input)=}")
