10977316adddfb9c33a0db48bacb37b4c66f9366
nihilok <mjfullstack@gmail.com>
2024-12-06 02:02:56 +0000

    Day 5 part 2

--- puzzles/day_5.py
+++ puzzles/day_5.py
@@ -20,6 +20,7 @@ def parse_input(puzzle_input):
 def part_1(puzzle_input):
     ordering, lines = parse_input(puzzle_input)
     correct_lines = []
+    
     for line in lines:
         correct_order = True
         for i, page in enumerate(line):
@@ -31,6 +32,7 @@ def part_1(puzzle_input):
         if correct_order:
             correct_lines.append(line)
     total = 0
+    
     for line in correct_lines:
         middle_item = line[len(line) // 2]
         total += int(middle_item)
@@ -38,7 +40,32 @@ def part_1(puzzle_input):
 
 
 def part_2(puzzle_input):
-    pass
+    ordering, lines = parse_input(puzzle_input)
+    incorrect_lines = []
+    
+    for line in lines:
+        for i, page in enumerate(line):
+            must_be_before = set(ordering[page])
+            pages_after = set(line[i + 1 :])
+            if must_be_before & pages_after:
+                incorrect_lines.append(line)
+                break
+
+    sorted_lines = []
+    for line in incorrect_lines:
+
+        def sort_key(x):
+            # Couldn't make this work until I saw this insane solution: https://topaz.github.io/paste/#XQAAAQDaAAAAAAAAAAA5HUm7ztOXp5yfdseppGCV6GxFTkRHwUEPU3ig/49z0PbvEWzToWGuJkqTW5Cv8C47QBCn/Pj2JeQMaflTZA8TlrZwHHZ11OjQ0e8kdywged5S4r+xgrUyKH7cRwlxe3DPo3OZCyk6yBhQf0HhtQSuRWxuNA+7fQQIgJr1Ls/7un7PXYuEs+xti9+dUqKrJ0hq0amZ8hcPsdgnVz7gtii8OxEjKha4k3yQ+ET/wND/73ISZw==
+            return -sum(x in ordering[page] for page in line)
+
+        sorted_line = sorted(line, key=sort_key)
+        sorted_lines.append(sorted_line)
+
+    total = 0
+    for line in sorted_lines:
+        middle_item = line[len(line) // 2]
+        total += int(middle_item)
+    return total
 
 
 def main():
